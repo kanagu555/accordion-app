a1bc47bf97063c7ec91ac76300e2bd10848022e0
Kanagaraj <kanagarajwhb@gmail.com>
2021-11-29 20:17:53 +0530

    added new tab functionality in navigation bar

---
+++ src/components/Link.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+const Link = ({className, href, children}) => {
+
+    const onClick = (event) => {
+
+        if(event.metaKey || event.ctrlKey) {
+            return;
+        }
+
+        event.preventDefault();
+        window.history.pushState({}, '', href)
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent)
+    }
+
+    return (
+        <div>
+            <a onClick={onClick} href={href} className={className}>
+                {children}
+            </a>
+        </div>
+    )
+}
+
+export default Link
