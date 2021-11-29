a704b24c7ae776d2ef81c47efed5186fb9aa870c
Kanagaraj <kanagarajwhb@gmail.com>
2021-11-29 19:25:17 +0530

    added navigation functionality between widgets app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
-    {
-        title: 'Whats is React?',
-        content: 'React is Front-end javascript library'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is popular JS library amount engineers '
-    },
-    {
-        title: 'How do you use React?',
-        content: 'By use the creating React components'
-    }
-]
+  {
+    title: "Whats is React?",
+    content: "React is Front-end javascript library",
+  },
+  {
+    title: "Why use React?",
+    content: "React is popular JS library amount engineers ",
+  },
+  {
+    title: "How do you use React?",
+    content: "By use the creating React components",
+  },
+];
 
+const options = [
+  {
+    label: "Red color",
+    value: "red",
+  },
+  {
+    label: "Green color",
+    value: "green",
+  },
+  {
+    label: "Blue color",
+    value: "blue",
+  },
+];
 
 const App = () => {
-    return (
-        <div>
-            {/* <Accordion items={items} /> */}
-            <Search />
-        </div>
-    )
-}
+  const [selected, setSelected] = useState(options[0]);
 
-export default App
+  return (
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
+};
+
+export default App;
